fcba8cb0ba42ec6be21b9d36e83219ac9a5cee2a
ayan4m1 <andrew@bulletlogic.com>
2023-02-10 00:22:26 -0500

    initial import

---
+++ ForBenchmark/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace ForBenchmark
+{
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [RPlotExporter]
+    public class ForLoops
+    {
+        private byte[] data;
+
+        [Params(10, 100, 1000, 10000, 100000)]
+        public int Items;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[Items];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public int Linq()
+        {
+            return data.Aggregate(0, (next, result) => result + next);
+        }
+
+        [Benchmark]
+        public int ForEach()
+        {
+            var sum = 0;
+
+            foreach (var item in data)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        [Benchmark]
+        public int ForIndex()
+        {
+            var sum = 0;
+
+            for (var index = 0; index < data.Length; index++)
+            {
+                var item = data[index];
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ForLoops>();
+        }
+    }
+}
